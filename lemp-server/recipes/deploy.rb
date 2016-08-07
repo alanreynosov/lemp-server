030c33e2b246451c1eee4dd6ada7802a32a79ada
Alan <alan@paradojo.com>
2016-08-07 18:19:10 -0500

    Added deploy recipe

--- lemp-server/recipes/deploy.rb
+++ lemp-server/recipes/deploy.rb
@@ -1,3 +1,10 @@
+#
+# Cookbook Name:: lemp-server
+# Recipe:: deploy
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
+
+
 deploy 'git_repo' do
   repo '#{deploy[:scm][:repository]}'
   user 'root'
