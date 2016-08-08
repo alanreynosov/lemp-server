023fae30c0029ada3ffe419a2e557154aa08e856
Alan <alan@paradojo.com>
2016-08-07 19:18:19 -0500

    fixed typo

--- lemp-server/recipes/deploy.rb
+++ lemp-server/recipes/deploy.rb
@@ -9,6 +9,6 @@ deploy 'git_repo' do
   repo '#{deploy[:scm][:repository]}'
   user 'root'
   deploy_to '/srv/www'
-  ssh_wrapper '#{[:deploy]["elpinerowp"][:scm][:ssh_key]}'
+  ssh_wrapper '#{node[:deploy]["elpinerowp"][:scm][:ssh_key]}'
   action :deploy
 end
