565b9ba91efdb5463d1e24ba8f98c30b529a3e3a
Alan <alan@paradojo.com>
2016-08-08 21:51:51 -0500

    added application_git cookbook

--- lemp-server/recipes/deploy.rb
+++ lemp-server/recipes/deploy.rb
@@ -5,10 +5,15 @@
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
 
-deploy 'git_repo' do
-  repo '#{deploy[:scm][:repository]}'
-  user 'root'
-  deploy_to '/srv/www'
-  ssh_wrapper node[:deploy]["elpinerowp"][:scm][:ssh_key]
-  action :deploy
+# deploy 'git_repo' do
+#   repo '#{deploy[:scm][:repository]}'
+#   user 'root'
+#   deploy_to '/srv/www'
+#   ssh_wrapper node[:deploy]["elpinerowp"][:scm][:ssh_key]
+#   action :deploy
+# end
+
+application_git '/srv/myapp' do
+  repository 'git@github.com:example/myapp.git'
+  deploy_key chef_vault_item('deploy', 'elpinerowp')[:scm][:ssh_key]
 end
