45d0704e7a1cae2865107b34a1fa74d71fd43c03
Alan <alan@paradojo.com>
2016-08-09 21:52:50 -0500

    typo in chef-vault

--- lemp-server/recipes/deploy.rb
+++ lemp-server/recipes/deploy.rb
@@ -5,8 +5,15 @@
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
 
-application '/srv/myapp' do
-  git 'git@github.com:example/myapp.git' do
+# deploy 'git_repo' do
+#   repo '#{deploy[:scm][:repository]}'
+#   user 'root'
+#   deploy_to '/srv/www'
+#   ssh_wrapper node[:deploy]["elpinerowp"][:scm][:ssh_key]
+#   action :deploy
+# end
+
+application '/srv/www' do
+  git '#{deploy[:scm][:repository]}' do
   	deploy_key chef_vault_item('deploy', 'elpinerowp')[:scm][:ssh_key]
-  end
 end
