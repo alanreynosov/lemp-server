c63642c789d7dff10e2d5b469937c66997a11c2f
Alan <alan@paradojo.com>
2016-08-09 20:25:33 -0500

    fixed deploy recipe

--- lemp-server/recipes/deploy.rb
+++ lemp-server/recipes/deploy.rb
@@ -5,15 +5,8 @@
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
 
-# deploy 'git_repo' do
-#   repo '#{deploy[:scm][:repository]}'
-#   user 'root'
-#   deploy_to '/srv/www'
-#   ssh_wrapper node[:deploy]["elpinerowp"][:scm][:ssh_key]
-#   action :deploy
-# end
-
-application_git '/srv/myapp' do
-  repository 'git@github.com:example/myapp.git'
-  deploy_key chef_vault_item('deploy', 'elpinerowp')[:scm][:ssh_key]
+application '/srv/myapp' do
+  git 'git@github.com:example/myapp.git'
+  	deploy_key chef_vault_item('deploy', 'elpinerowp')[:scm][:ssh_key]
+  end
 end
