7eb8b1161c5845295eff8fe9e76dba0705bcbb1e
Alan Reynoso <alan@paradojo.com>
2016-08-13 23:24:25 -0500

    added key to ssh wrapper

--- lemp-server/attributes/default.rb
+++ lemp-server/attributes/default.rb
@@ -1,4 +1,4 @@
 default[:nginx_user] = 'www-data'
 default[:nginx_group] = 'www-data'
-default[:nginx_document_root] = '/srv/www/elpinero-wp/'
+default[:nginx_document_root] = '/srv/www/'
 default[:nginx_host_name] = 'elpinerodelacuenca.com.mx'
