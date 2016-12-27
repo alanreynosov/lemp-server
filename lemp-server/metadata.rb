09693069ba24dae04f061d88b8b236d12434c459
Alan Reynoso <alan@paradojo.com>
2016-12-27 17:44:13 -0600

    cleanup cached

--- lemp-server/metadata.rb
+++
@@ -1,9 +0,0 @@
-name 'lemp-server'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
-license 'all_rights'
-description 'Installs/Configures lemp-server'
-long_description 'Installs/Configures lemp-server'
-version '0.1.0'
-
-depends "apt"
