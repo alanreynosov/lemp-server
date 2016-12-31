eb8a73e6b1f89346f58c7ecc71ba2962f0656e24
Alan Reynoso <alan@paradojo.com>
2016-12-31 17:26:52 -0600

    fixed typo for document root

--- lemp-server/recipes/update_assets_cdn.rb
+++ lemp-server/recipes/update_assets_cdn.rb
@@ -9,6 +9,6 @@ app = search("aws_opsworks_app").first
 bash 'update_assets' do
   code <<-EOH
     export AWS_ACCESS_KEY_ID=#{app[:environment][:AWS_ACCESS_KEY_ID]}; export AWS_SECRET_ACCESS_KEY=#{app[:environment][:AWS_SECRET_ACCESS_KEY]};
-    aws s3 sync /srv/www/#{newbuild} s3://#{app[:environment][:assets_bucket ]}/ --exclude="*.php" --exclude="*.html" --exclude="*.gz" --exclude=".git/*" --exclude=".htaccess" --exclude="*.txt" --exclude=".gitignore"
+    aws s3 sync /srv/www/current s3://#{app[:environment][:assets_bucket ]}/ --exclude="*.php" --exclude="*.html" --exclude="*.gz" --exclude=".git/*" --exclude=".htaccess" --exclude="*.txt" --exclude=".gitignore"
   EOH
 end
