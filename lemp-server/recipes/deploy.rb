4594114da91e10f966cd5ed52a651480f506ad0a
Alan Reynoso <alan@paradojo.com>
2017-02-08 20:47:09 -0600

    remove asset updates

--- lemp-server/recipes/deploy.rb
+++ lemp-server/recipes/deploy.rb
@@ -63,12 +63,12 @@ bash 'unzip_build' do
   notifies :run, 'execute[chown-data-www]', :immediately
 end
 
-bash 'update_assets' do
-  code <<-EOH
-    export AWS_ACCESS_KEY_ID=#{app[:environment][:AWS_ACCESS_KEY_ID]}; export AWS_SECRET_ACCESS_KEY=#{app[:environment][:AWS_SECRET_ACCESS_KEY]};
-    aws s3 sync s3://#{app[:environment][:assets_bucket ]}/* /srv/www/#{newbuild} --exclude="*.php" --exclude="*.html" --exclude="*.gz" --exclude=".git/*" --exclude=".htaccess" --exclude="*.txt" --exclude=".gitignore"
-  EOH
-end
+#bash 'update_assets' do
+#  code <<-EOH
+#    export AWS_ACCESS_KEY_ID=#{app[:environment][:AWS_ACCESS_KEY_ID]}; export AWS_SECRET_ACCESS_KEY=#{app[:environment][:AWS_SECRET_ACCESS_KEY]};
+#    aws s3 sync s3://#{app[:environment][:assets_bucket ]}/* /srv/www/#{newbuild} --exclude="*.php" --exclude="*.html" --exclude="*.gz" --exclude=".git/*" --exclude=".htaccess" --exclude="*.txt" --exclude=".gitignore"
+#  EOH
+#end
 
 
 execute "chown-data-www" do
